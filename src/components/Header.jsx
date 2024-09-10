1f0aa58c62cc2aec8b9499632c99556618495786
Vadim Kh <vadim.khamnuev@gmail.com>
2024-09-10 08:13:01 +0300

    clean & format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
-import 
-
 export const Header = () => {
-    return (
-      <header>
-        <div>
-          <h2>Здравствуйте!</h2>
-          <div>
-            
-          </div>
-        </div>
-      </header>
-    );
-  };
-  export default Header;
-  
+  return (
+    <header>
+      <div>
+        <h2>Здравствуйте!</h2>
+        <div></div>
+      </div>
+    </header>
+  );
+};
+export default Header;
